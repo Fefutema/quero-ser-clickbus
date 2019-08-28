b222f80b23088ba83506b33944af21b1788391d1
Fefutema <felipe.fut09@hotmail.com>
2019-08-27 23:23:57 -0300

    Initial commit

---
+++ testes/backend-developer/solution/clickbus-api/src/test/java/br/com/futema/ClickbusApiApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.futema;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ClickbusApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
